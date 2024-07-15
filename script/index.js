b192ca3552b7aa581e28059f6056976356306853
kozzztia <kozzztia@gmail.com>
2024-07-15 10:37:59 +0300

    timer + styled links

---
+++ script/index.js
@@ -0,0 +1,13 @@
+
+function updateTime() {
+    const timeElement = document.querySelector('.time');
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    timeElement.textContent = `${hours}:${minutes}:${seconds}`;
+}
+
+updateTime();
+
+setInterval(updateTime, 1000);
